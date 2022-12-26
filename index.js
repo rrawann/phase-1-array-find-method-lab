bbfc3fffc29ab1d2e4a3977e34b9f6c2d073bb35
Rawan <rawnq.sb@gmail.com>
2022-12-26 23:07:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+
+superbowlWin = (record) => {
+
+    const result = record.find( record => record.result === "W" );
+    
+    if(result)
+        return result.year;
+  }
